1ba057b2c510d92493533f5fa5201939602b1f6d
Fernandes Alves Fabien <fabien@code42.fr>
2022-03-09 07:11:47 +0100

    Added : Dock to the app

---
+++ src/constants/images.js
@@ -0,0 +1,35 @@
+import appstore from "../assets/images/appstore.png";
+import contacts from "../assets/images/contacts.png";
+import facetime from "../assets/images/facetime.png";
+import finder from "../assets/images/finder.png";
+import launchpad from "../assets/images/launchpad.png";
+import messages from "../assets/images/messages.png";
+import music from "../assets/images/music.png";
+import notes from "../assets/images/notes.png";
+import photos from "../assets/images/photos.png";
+import podcasts from "../assets/images/podcasts.png";
+import reminders from "../assets/images/reminders.png";
+import safari from "../assets/images/safari.png";
+import siri from "../assets/images/siri.png";
+import trash from "../assets/images/trash.png";
+import tv from "../assets/images/tv.png";
+
+const exportedImages = {
+  appstore,
+  contacts,
+  facetime,
+  finder,
+  launchpad,
+  messages,
+  music,
+  notes,
+  photos,
+  podcasts,
+  reminders,
+  safari,
+  siri,
+  trash,
+  tv,
+};
+
+export default exportedImages;
