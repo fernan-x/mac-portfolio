d982857ece787e19467ebf2623c687d902741949
Fernandes Alves Fabien <fabien@code42.fr>
2022-03-09 15:40:46 +0100

    WIP : Start wreating draggable window layout

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import Dock from "./components/Dock/Dock";
+import Window from "./layouts/Window/Window";
 
 function App() {
   return (
     <div className="App">
+      <Window width={600} height={400} open />
+      {/* <Window width={400} height={300} open /> */}
       <Dock />
     </div>
   );
