e3b172caef777295ba72cfd7f73aae394cf29c66
Fernandes Alves Fabien <fabien@code42.fr>
2022-03-10 10:13:10 +0100

    Changed : Refactor and change the menu bar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import Dock from "./components/Dock/Dock";
+import MenuBar from "./components/MenuBar/MenuBar";
 import Window from "./layouts/Window/Window";
 
 function App() {
   return (
     <div className="App">
+      <MenuBar />
       <Window width={600} height={400} open />
       {/* <Window width={400} height={300} open /> */}
       <Dock />
--- src/components/Dock/Dock.jsx
+++ src/components/Dock/Dock.jsx
@@ -64,77 +64,21 @@ const DockEntry = ({ id, index, name, img, classes, last }) => {
 
 const Dock = () => {
   return (
-    <>
-      <div className="menu-bar">
-        <div className="left">
-          <img
-            src="https://upload.wikimedia.org/wikipedia/commons/thumb/3/31/Apple_logo_white.svg/1010px-Apple_logo_white.svg.png"
-            className="apple-logo"
-            alt=""
+    <div className="dock">
+      <div className="dock-container">
+        {constants.dockEntries.map((entry, idx) => (
+          <DockEntry
+            id={entry.id}
+            name={entry.name}
+            img={entry.img}
+            key={idx + 1}
+            index={idx + 1}
+            classes={entry.classes}
+            last={entry.last}
           />
-          <span className="menus active">Finder</span>
-          <span className="menus">File</span>
-          <span className="menus">Edit</span>
-          <span className="menus">View</span>
-          <span className="menus">Go</span>
-          <span className="menus">Window</span>
-          <span className="menus">Help</span>
-        </div>
-        <div className="right">
-          <div className="menu-ico">
-            <img
-              src="https://freepngimg.com/download/united_states/76187-sound-information-united-business-states-address-email.png"
-              alt=""
-              className="vol"
-            />
-          </div>
-          <div className="menu-ico">
-            <i className="fab fa-bluetooth-b"></i>
-          </div>
-          <div className="menu-ico">
-            <i className="fas fa-battery-half"></i>
-          </div>
-          <div className="menu-ico">
-            <i className="fas fa-wifi"></i>
-          </div>
-          <div className="menu-ico">
-            <i className="fas fa-search"></i>
-          </div>
-          <div className="menu-ico">
-            <img
-              src="https://eshop.macsales.com/blog/wp-content/uploads/2021/03/control-center-icon.png"
-              alt=""
-              className="control-center"
-            />
-          </div>
-          <div className="menu-ico">
-            <img
-              src="https://upload.wikimedia.org/wikipedia/en/8/8e/AppleSiriIcon2017.png"
-              alt=""
-              className="siri"
-            />
-          </div>
-
-          <div className="menu-time">Mon 31 May 05:30</div>
-        </div>
+        ))}
       </div>
-
-      <div className="dock">
-        <div className="dock-container">
-          {constants.dockEntries.map((entry, idx) => (
-            <DockEntry
-              id={entry.id}
-              name={entry.name}
-              img={entry.img}
-              key={idx + 1}
-              index={idx + 1}
-              classes={entry.classes}
-              last={entry.last}
-            />
-          ))}
-        </div>
-      </div>
-    </>
+    </div>
   );
 };
 
