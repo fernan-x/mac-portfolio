c544959eb7563f9f157aeea1860de371b4615f18
Fabien Fernandes Alves <fwfabou@gmail.com>
2025-01-15 15:33:08 +0100

    chore: remove unused packages [#54]

--- src/index.jsx
+++ src/index.jsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom";
 import "./assets/styles/styles.scss";
 import "./services/translation.js";
 import App from "./App";
-import reportWebVitals from "./reportWebVitals";
+
 import { Provider } from "react-redux";
 import { store } from "./store/store";
 
@@ -15,8 +15,3 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById("root")
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
